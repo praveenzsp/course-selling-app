c58c54cb439d20b595673365e244ba549af0cf3d
Znanam Sai Praveen <znanam_saipraveen@comcast.com>
2025-01-05 00:28:41 +0530

    added prisma client and first migration

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import express from 'express';
-import { Request, Response } from 'express';
-import authRouter from './routes/auth';
+import express from "express";
+import { Request, Response } from "express";
+import authRouter from "./routes/auth";
+import prisma from "./db";
+import cors from "cors";
 const app = express();
+const port=process.env.PORT || 8080;
 
-app.get('/', (req:Request, res: Response) => {
-  res.send('Hello World');
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  const allUsers = prisma.user.findMany();
+  res.send(allUsers);
 });
 
-app.use('/auth', authRouter);
+app.use("/auth", authRouter);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
 });
